6ebc66efb9061fd455c8a710f578fefe63a03b29
Mix <paradorn.rinrit.m@gmail.com>
2025-05-24 21:23:01 +0700

    [mix] "show map with google and find location gps with geolocator"

--- lib/app/ui/home/home.dart
+++ lib/app/ui/home/home.dart
@@ -1,65 +1,97 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_map_test/app/ui/home/home_controller.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Home extends StatelessWidget {
-  Home({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final controller = Get.put(HomeController());
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 40),
-            child: Column(
-              children: <Widget>[
-                Obx(() {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: TextField(
-                      controller: controller.controller,
-                      focusNode: controller.focusNode,
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        contentPadding: EdgeInsets.only(left: 20),
-                        suffixIcon: controller.isFocused.value
-                            ? IconButton(
-                                padding: EdgeInsets.zero,
-                                icon: Icon(Icons.close),
-                                onPressed: () => log("close"),
-                              )
-                            : SizedBox(),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(32),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(32),
-                        ),
-                      ),
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Obx(() => SizedBox.expand(
+                child: GoogleMap(
+                  onMapCreated: (controllerOnMap) =>
+                      controller.mapController = controllerOnMap,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(
+                      controller.latitude.value,
+                      controller.longitude.value,
                     ),
-                  );
-                }),
-                Obx(() {
-                  return Text(
-                    controller.counter.toString(),
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  );
-                }),
-              ],
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: controller.incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ),
+                    zoom: 16.0,
+                  ),
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: true,
+                ),
+              )),
+          Obx(() {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 40),
+              child: TextField(
+                controller: controller.controller,
+                focusNode: controller.focusNode,
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  contentPadding: EdgeInsets.only(left: 20),
+                  filled: true,
+                  fillColor: Colors.white,
+                  suffixIcon: controller.isFocused.value
+                      ? IconButton(
+                          padding: EdgeInsets.zero,
+                          icon: Icon(Icons.close),
+                          onPressed: () => log("close"),
+                        )
+                      : SizedBox(),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                ),
+              ),
+            );
+          }),
+        ],
       ),
+      // floatingActionButton: FloatingActionButton(onPressed: () {
+      //   controller.determinePosition();
+      // }),
+      // bottomSheet: Container(
+      //   width: MediaQuery.of(context).size.width,
+      //   height: MediaQuery.of(context).size.height * 0.75,
+      //   padding: EdgeInsets.all(16.0),
+      //   decoration: BoxDecoration(
+      //     borderRadius: BorderRadius.only(
+      //       topLeft: Radius.circular(16),
+      //       topRight: Radius.circular(16),
+      //     ),
+      //     color: Colors.white,
+      //     boxShadow: [
+      //       BoxShadow(
+      //         color: Colors.grey.shade600,
+      //         spreadRadius: 1,
+      //         blurRadius: 15,
+      //       ),
+      //     ],
+      //   ),
+      //   child: Text('data'),
+      // ),
     );
   }
 }
--- lib/app/ui/home/home_controller.dart
+++ lib/app/ui/home/home_controller.dart
@@ -1,31 +1,82 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeController extends GetxController {
   final TextEditingController controller = TextEditingController();
+  late GoogleMapController mapController;
   final FocusNode focusNode = FocusNode();
   var isFocused = false.obs;
+  var latitude = 0.0.obs;
+  var longitude = 0.0.obs;
 
   @override
   void onInit() {
     super.onInit();
-    focusNode.addListener(_handleFocusChange);
+    determinePosition();
+    focusNode.addListener(handleFocusChange);
   }
 
   @override
   void onClose() {
     controller.dispose();
-    focusNode.removeListener(_handleFocusChange);
+    focusNode.removeListener(handleFocusChange);
     focusNode.dispose();
     super.onClose();
   }
 
-  void _handleFocusChange() {
-    isFocused.value = focusNode.hasFocus; // อัปเดตแบบนี้ถูกต้อง
+  // ใช้แสดง suffixIcon ใน TextField
+  void handleFocusChange() {
+    isFocused.value = focusNode.hasFocus;
   }
 
-  var counter = 0.obs;
-  void incrementCounter() {
-    counter++;
+  // ดึงข้อมูล Location
+  Future<void> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    //เปิด Location หรือยัง
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    // ตรวจสอบสิทธิ์
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+    );
+
+    Position position = await Geolocator.getCurrentPosition(
+      locationSettings: locationSettings,
+    );
+
+    latitude.value = position.latitude;
+    longitude.value = position.longitude;
+    log("${latitude.value}, ${longitude.value}");
+
+    moveCamera();
+  }
+
+  void moveCamera() {
+    if (mapController != null) {
+      final pos = LatLng(latitude.value, longitude.value);
+      mapController.animateCamera(CameraUpdate.newLatLng(pos));
+    }
   }
 }
